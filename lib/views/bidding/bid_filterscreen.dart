0772b2bd8a66d4f082262f582045f68fdbb92844
si-moeez-suleman <97084719+si-moeez-suleman@users.noreply.github.com>
2022-09-23 08:13:41 -0700

    in progreess filter screen

--- lib/views/bidding/bid_filterscreen.dart
+++ lib/views/bidding/bid_filterscreen.dart
@@ -66,7 +66,7 @@ class BidFilterScreen extends StatelessWidget {
                           height: 5,
                           width: Get.width / 2.0,
                           color: AppColors.whiteColor,
-                        )
+                        ),
                       ],
                     ),
                   ],
@@ -77,42 +77,59 @@ class BidFilterScreen extends StatelessWidget {
                 color: AppColors.textColor,
                 fontWeight: FontWeight.w600,
               ).marginOnly(top: 22, bottom: 16, left: 21, right: 16),
-              const MyText(
-                AppTexts.vechiclesData,
-                color: AppColors.textColor,
-                fontWeight: FontWeight.w600,
-              ).marginOnly(top: 22, bottom: 16, left: 21, right: 16),
-              Center(
-                  child: MultiSelectDropDown(
-                backgroundColor: AppColors.whiteColor,
-                hint: "Select Brand",
-                hintFontSize: 15,
-                hintColor: AppColors.textfieldColor,
-                selectedOptionTextColor: AppColors.blackColor,
-                selectedOptionBackgroundColor: AppColors.transparentColor,
-                optionsBackgroundColor: AppColors.transparentColor,
-                onOptionSelected: (List<ValueItem> selectedOptions) {},
-                options: const <ValueItem>[
-                  ValueItem(label: 'Option 1', value: '1'),
-                  ValueItem(label: 'Option 2', value: '2'),
-                  ValueItem(label: 'Option 3', value: '3'),
-                  ValueItem(label: 'Option 4', value: '4'),
-                  ValueItem(label: 'Option 5', value: '5'),
-                  ValueItem(label: 'Option 6', value: '6'),
-                ],
-                selectionType: SelectionType.multi,
-                chipConfig: const ChipConfig(wrapType: WrapType.wrap),
-                
-                dropdownHeight: 300,
-                optionTextStyle:
-                    const TextStyle(fontSize: 16, color: AppColors.blackColor),
-                selectedOptionIcon: const Icon(
-                  Icons.check_circle,
-                  color: AppColors.buttonColor,
-                ),
-              )).marginAll(16)
+              commonDropdown([
+                const ValueItem(label: 'Option 1', value: '1'),
+                const ValueItem(label: 'Option 2', value: '2'),
+                const ValueItem(label: 'Option 3', value: '3'),
+                const ValueItem(label: 'Option 4', value: '4'),
+                const ValueItem(label: 'Option 5', value: '5'),
+                const ValueItem(label: 'Option 6', value: '6'),
+              ], _, "Brands", "Select Brands"),
+              commonDropdown([
+                const ValueItem(label: 'Option 1', value: '1'),
+                const ValueItem(label: 'Option 2', value: '2'),
+                const ValueItem(label: 'Option 3', value: '3'),
+                const ValueItem(label: 'Option 4', value: '4'),
+                const ValueItem(label: 'Option 5', value: '5'),
+                const ValueItem(label: 'Option 6', value: '6'),
+              ], _, "Model", "Select Modal")
             ],
           );
         });
+  }
+
+  Widget commonDropdown(List<ValueItem> options, FilterscreenController _,
+      String title, String hintText) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        MyText(title,
+                color: AppColors.textfieldLabelColor,
+                fontSize: 10,
+                fontWeight: FontWeight.w400)
+            .marginOnly(left: 32),
+        Center(
+            child: MultiSelectDropDown(
+          backgroundColor: AppColors.whiteColor,
+          hint: hintText,
+          hintFontSize: 15,
+          hintColor: AppColors.textfieldColor,
+          selectedOptionTextColor: AppColors.blackColor,
+          selectedOptionBackgroundColor: AppColors.transparentColor,
+          optionsBackgroundColor: AppColors.transparentColor,
+          onOptionSelected: (List<ValueItem> selectedOptions) {},
+          options: options,
+          selectionType: SelectionType.multi,
+          chipConfig: const ChipConfig(wrapType: WrapType.wrap),
+          dropdownHeight: 300,
+          optionTextStyle:
+              const TextStyle(fontSize: 16, color: AppColors.blackColor),
+          selectedOptionIcon: const Icon(
+            Icons.check_circle,
+            color: AppColors.buttonColor,
+          ),
+        )).marginAll(16)
+      ],
+    );
   }
 }
